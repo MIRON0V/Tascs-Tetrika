942bcd74242cfa4b9c7947d0686337dd664be243
Aleksey Mironov <russian.bomond@gmail.com>
2020-08-17 03:53:05 +0300

    Задание 3: Оценка уроков

--- Assessment_of_lessons.sql
+++ Assessment_of_lessons.sql
@@ -1,20 +1,22 @@
-﻿SELECT date(scheduled_time) AS ride_date, user_id, ROUND((SUM(grade)/COUNT(user_id)),2) AS average_score
+﻿SELECT ride_date, user_id, MIN(average_score)
 
-FROM  (SELECT user_id, role, event_id
-       FROM test_db.users INNER JOIN  test_db.participants
-       ON users.id = participants.user_id
-       WHERE role = "tutor"
-       GROUP BY event_id) event_participants
-       INNER JOIN lessons
-       ON lessons.event_id = event_participants.event_id 
-       INNER JOIN 
-      (SELECT lesson_id, (tech_quality / COUNT(lesson_id)) AS grade 
-       FROM test_db.quality 
-       WHERE tech_quality != 0 
-       GROUP BY  lesson_id) grades
-       ON lessons.id = grades.lesson_id 
+FROM (SELECT date(scheduled_time) AS ride_date, user_id, ROUND((SUM(grade)/COUNT(user_id)),2) AS average_score
+      FROM  (SELECT user_id, role, event_id
+             FROM test_db.users INNER JOIN  test_db.participants
+             ON users.id = participants.user_id
+             WHERE role = "tutor"
+             GROUP BY event_id) event_participants
+             INNER JOIN lessons
+             ON lessons.event_id = event_participants.event_id 
+             INNER JOIN 
+            (SELECT lesson_id, (tech_quality / COUNT(lesson_id)) AS grade 
+             FROM test_db.quality 
+             WHERE tech_quality != 0 
+             GROUP BY  lesson_id) grades
+             ON lessons.id = grades.lesson_id 
 
-WHERE subject = 'phys'
-GROUP BY ride_date, user_id
+             WHERE subject = 'phys'
+             GROUP BY ride_date, user_id
+             ORDER BY ride_date, average_score)query_in 
 
-ORDER BY ride_date, average_score
+GROUP BY ride_date
